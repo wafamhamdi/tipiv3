f073003bb960b8b9112feae15fbcd58c38e18d6a
wafa <wafa.mhamdi.jamel@gmail.com>
2018-04-17 12:38:16 +0100

    verif

--- config.js
+++ config.js
@@ -2,11 +2,8 @@ module.exports=
 {
     jwt_secret:process.env.jwt_secret||'My app',
     port = process.env.PORT || '3000',
-<<<<<<< HEAD
+//<<<<<<< HEAD
     mongo_uri:process.env.mongo_uri||'mongodb://tipi:tipi@ds241489.mlab.com:41489/tipigit'
-=======
-    mongo_uri:process.env.mongo_uri||'mongodb://wafamhamdi:wafawafa20@ds145283.mlab.com:45283/tipigit'
->>>>>>> 48905567104fbbf7a1222971e761c501c1cf3f3b
 
 
 }
